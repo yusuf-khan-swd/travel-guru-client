6602f63c29dc4385225ca3e7337d830f9bd12f5a
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-22 19:27:01 +0600

    added logo and basic style

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/brand/logo.png';
 
 const Header = () => {
   return (
-    <nav>
-      <Link to='/home'>Home</Link>
-      <Link to='/login'>login</Link>
-      <Link to='/register'>Register</Link>
+    <nav className='flex justify-between items-center pr-14 pl-14'>
+      <div>
+        <Link to='/'>
+          <img className='w-32' src={logo} alt="" />
+        </Link>
+      </div>
+      <div>
+        <Link className='mr-4 hover:text-orange-400' to='/home'>Home</Link>
+        <Link className='mr-4 hover:text-orange-400' to='/login'>login</Link>
+        <Link className='mr-4 hover:text-orange-400' to='/register'>Register</Link>
+      </div>
     </nav>
   );
 };
