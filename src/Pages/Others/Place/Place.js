9a2d6a7c60c568735f284c019088b515381b92f2
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-23 19:15:25 +0600

    remove extra css

--- src/Pages/Others/Place/Place.js
+++ src/Pages/Others/Place/Place.js
@@ -4,17 +4,13 @@ import { Link } from 'react-router-dom';
 const Place = ({ place }) => {
   const { name, img, placesId } = place;
   return (
-    <div className="relative">
+    <div className='text-center'>
       <img
         src={img}
-        className="absolute h-full"
+        className="h-96 mx-auto"
         alt=""
       />
-      <div className="relative h-96" >
-        <h2 className="z-10 p-5">
-          <Link to={`/hotels/${placesId}`} className="font-bold text-3xl hover:underline">{name}</Link>
-        </h2>
-      </div>
+      <Link to={`/hotels/${placesId}`} className="font-bold text-3xl hover:underline ">{name}</Link>
     </div>
   );
 };
