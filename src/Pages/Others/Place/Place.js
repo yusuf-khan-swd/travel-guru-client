85d20e35a62c1629a9a68b7c3e85d46942123164
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-23 22:46:30 +0600

    added react calender to project

--- src/Pages/Others/Place/Place.js
+++ src/Pages/Others/Place/Place.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Calendar } from 'react-calendar';
 import { Link, useLoaderData } from 'react-router-dom';
+import 'react-calendar/dist/Calendar.css';
 
 const Place = () => {
+  const [value, onChange] = useState(new Date());
   const places = useLoaderData();
   const { name, placesId } = places;
 
   return (
     <div>
+      <Calendar onChange={onChange} value={value} />
       <h2 className='text-3xl uppercase mb-5'>{name}</h2>
       <Link to={`/hotels/${placesId}`} className='px-2 py-2 bg-orange-400 rounded-md '>Start Booking</Link>
     </div>
